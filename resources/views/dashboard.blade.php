3d842f39787bd7986fba8d86b12df7d718dd6a58
Bryan  Andion <bryan.andion@gmail.com>
2024-06-26 21:27:15 -0500

    mm

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -65,7 +65,8 @@
                                                     @csrf
                                                     <a href="{{ route('personals.edit', $personal->id) }}"
                                                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Editar</a>
-                                                        <button type="submit"  class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Borrar</button>
+                                                    <button type="submit"
+                                                        class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Borrar</button>
                                                 </form>
                                             </td>
                                         </tr>
